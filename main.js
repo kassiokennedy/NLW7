63bf73a5b19ca77f0683537c4dc0ab3c5eb9bd1c
kassiokennedy <kassio1206@hotmail.com>
2022-02-08 19:26:49 -0300

    NLW7 video3 ok

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const link = {
+const linkSocialMedia = {
   github: 'kassiokennedy',
   youtube: 'UCavEwESz3925fFil-kZU_1Q',
-  instagram: 'kassioksvieira'
+  facebook: 'facebook',
+  instagram: 'kassioksvieira',
+  twitter: 'twitter'
 }
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linkSocialMedia[social]}`
+    //alert(li.children[0].href)
+  }
+}
+changeSocialMediaLinks()
